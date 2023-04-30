ed21d16e831abd5c0c2cf388841310a84d172b39
Luciano Menezes <lucianomenezes13@gmail.com>
2023-04-30 12:25:09 -0700

    Added docBlock to the PHP functions

--- private/functions.php
+++ private/functions.php
@@ -1,7 +1,16 @@
 <?php
 
-
-// Get user's address information from the user table
+/**
+ * Get user's address information from the user table.
+ *
+ * @param int $user_id The ID of the user.
+ * @return array An associative array containing the user's address information:
+ *               - 'street_1': The first line of the street address.
+ *               - 'street_2': The second line of the street address.
+ *               - 'city': The city name.
+ *               - 'state_abbrev': The abbreviation of the state.
+ *               - 'zip_code': The ZIP code.
+ */
 function find_user_address_by_id($user_id)
 {
   global $connection;
@@ -22,7 +31,16 @@ function find_user_address_by_id($user_id)
   );
 }
 
-//calculate the cart total
+/**
+ * Calculate the total price and quantity of items in the cart.
+ *
+ * This function calculates the total price and quantity of items in the cart
+ * based on the product prices and quantities stored in the session variable 'cart'.
+ * The calculated total price is stored in the session variable 'total',
+ * and the total quantity is stored in the session variable 'quantity'.
+ *
+ * @return void
+ */
 function calculateTotalCart()
 {
 
@@ -41,7 +59,18 @@ function calculateTotalCart()
   $_SESSION['quantity'] = $total_quantity;
 }
 
-//Calculate the total price of the order
+/**
+ * Calculate the total price of the order.
+ *
+ * This function calculates the total price of the order based on the item prices
+ * and quantities provided in the $order_details array.
+ *
+ * @param array $order_details An array containing the details of the order.
+ *                            Each element should be an associative array with
+ *                            'item_price' and 'quantity' keys.
+ *
+ * @return float The total price of the order.
+ */
 function calculateTotalOrderPrice($order_details)
 {
 
@@ -56,7 +85,18 @@ function calculateTotalOrderPrice($order_details)
   return $total;
 }
 
-// Escape special characters to prevent XSS attacks
+/**
+ * Escape special characters to prevent XSS attacks.
+ *
+ * This function takes a string as input and applies the htmlspecialchars function
+ * to escape special characters that have special meaning in HTML, thus preventing
+ * potential cross-site scripting (XSS) attacks. The string is encoded using the
+ * 'UTF-8' character encoding and the ENT_QUOTES flag is set to convert both
+ * double and single quotes.
+ *
+ * @param string $string The string to be escaped.
+ * @return string The escaped string.
+ */
 function h($string)
 {
   return htmlspecialchars($string, ENT_QUOTES, 'UTF-8');
